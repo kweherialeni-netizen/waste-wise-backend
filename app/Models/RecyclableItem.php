098cc2ceae863c9fca64a8c135a74939d4ee886b
unknown <kweherialeni@gmail.com>
2025-11-27 10:56:52 +0300

    creation of auth

---
+++ app/Models/RecyclableItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RecyclableItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\RecyclableItemFactory> */
+    use HasFactory;
+}
